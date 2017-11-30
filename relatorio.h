ddf7d06074889ca490bd50cb37e70d0bf40a7e49
Christian Elster <christianelster@infojr.com.br>
2017-11-30 11:58:27 -0300

    Comenta

--- relatorio.h
+++ relatorio.h
@@ -1,8 +1,12 @@
 #ifndef _RELATORIO_H
 #define _RELATORIO_H
 
+//inicializa structs de nó e cliente
+
 typedef struct node Node;
 typedef struct cliente Cliente;
+
+//inicializa funções
 
 void push_relatorio(Cliente a);
 void relatorio();
